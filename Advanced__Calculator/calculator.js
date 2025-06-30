9e96982a84a923fbfcd667266d6e95aae218d466
LimoHezron <kibungeihezron@gmail.com>
2025-06-30 13:40:48 +0300

    second commit

---
+++ Advanced__Calculator/calculator.js
@@ -0,0 +1,57 @@
+const buttonValues = [
+    "AC","+/-","%","÷",
+    "7","8","9","x",
+    "4","5","6","-",
+    "1","2","3","+",
+    "0",".","="
+];
+const rightSymbols = ["÷","x","-","+","="]
+const topSymbols = ["AC","+/-","%"]
+
+document.getElementById('display')
+let A = 0;
+let operator = null;
+let B = null;
+
+for (let i = 0; i < buttonValues.length; i++) {
+    let value = buttonValues[i]; 
+    let button = document.createElement("button"); //creating the button tag <button></button>
+    button.innerText = value;
+    // stylling the buttons
+    if(value === "0"){
+        button.style.width = "200px"
+        button.style.gridColumn = "span 2"
+    }
+    if (rightSymbols.includes(value)){
+        button.style.backgroundColor = "#ff9500";
+    }
+    else if (topSymbols.includes(value)){
+        button.style.backgroundColor = "#d4d4d2";
+        button.style.color = "#1c1c1c"
+    }
+
+    button.addEventListener('click', function(){
+        if (rightSymbols.includes(value)) {
+
+        }
+        else if (topSymbols.includes(value)){
+
+        }
+        else {
+            if(value === "."){
+                if(display.value !== "" && !display.value.includes(value)){
+                    display.value += value;
+                }
+            }
+            else if (display.value === "0"){
+                display.value = value;
+            }
+            else{
+                display.value += value;
+            }
+        }
+    })
+    // Add buttons to the calculator
+    document.getElementById('buttons').appendChild(button);
+    
+}
