e43914885a8e041b1ffac7ef0950a87b7b07bed3
LimoHezron <kibungeihezron@gmail.com>
2025-10-30 13:04:19 +0300

    Local Storage added to my todo list

--- To-do List/script.js
+++ To-do List/script.js
@@ -1,13 +1,21 @@
 const itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem('items')) : []
 console.log(itemsArray);
 
+document.querySelector('#enter').addEventListener('click', ()=>{
+    const item = document.querySelector('#item')
+    createItem(item)
+})
+function createItem(item){
+    itemsArray.push(item.value)
+    localStorage.setItem('items',JSON.stringify(itemsArray))
+    location.reload()
 
+}
 
 function displayDate(){
     let date = new Date()
     date = date.toString().split(" ")
     document.querySelector('#date').innerHTML = date[1] + " " + date[2] + " " + date[3]
-    console.log(date)
 }
 window.onload = function(){
     displayDate()
