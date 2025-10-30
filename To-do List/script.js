853a7d24ce447f4959c2314224e3a9ee4eeee2cf
LimoHezron <kibungeihezron@gmail.com>
2025-10-30 18:33:51 +0300

    Items inputed by the user dispalyed in the Screen

--- To-do List/script.js
+++ To-do List/script.js
@@ -4,13 +4,36 @@ console.log(itemsArray);
 document.querySelector('#enter').addEventListener('click', ()=>{
     const item = document.querySelector('#item')
     createItem(item)
-})
+});
+function displayItem(){
+    let items = "";
+    for(let i = 0;i < itemsArray.length; i++){
+items += `<div class="item">
+                <div class="input-controller">
+                    <textarea disabled>${itemsArray[i]}</textarea>
+                    <div class="edit-controller">
+                        <i class="fa-solid fa-trash deleteBtn"></i>
+                        <i class="fa-solid fa-pen-to-square editBtn"></i>
+                    </div>
+                </div>
+                <div class="update-controller">
+                    <button class="saveBtn">Save</button>
+                    <button class="cancelBtn">Cancel</button>
+                </div>
+            </div>`
+    }
+    document.querySelector('.to-do-list').innerHTML = items;
+  
+};
+
+
+
 function createItem(item){
     itemsArray.push(item.value)
     localStorage.setItem('items',JSON.stringify(itemsArray))
     location.reload()
 
-}
+};
 
 function displayDate(){
     let date = new Date()
@@ -19,4 +42,6 @@ function displayDate(){
 }
 window.onload = function(){
     displayDate()
+    displayItem()
 }
+    
